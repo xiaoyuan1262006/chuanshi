59c5437c707cdd1d5e779a686937d16d78fddde6
kangzw <37102524+CSkangzhiwei@users.noreply.github.com>
2018-03-08 10:42:33 +0800

    Add files via upload
    增加2个表：充值，充值及消费记录；

--- badminton/cost/models.py
+++ badminton/cost/models.py
@@ -1,11 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Place(models.Model):
     name=models.CharField('地点',max_length=50,null=True,blank=True,default=None)
-    
+
+    def __str__(self):
+        return '%s ' % (self.name)
+
+    class Meta:
+        verbose_name = '地点'
+        verbose_name_plural = verbose_name
+
+
 class Event(models.Model):
     time=models.DateField('时间',null=True,blank=True,default=None)
-    place=models.ForeignKey(verbose_name='地点',null=True,blank=True,default=None)
-    members=models.ManyToManyField(ManyToManyField,verbose_name='参与人',null=True,blank=True,default=None)
+    place=models.ForeignKey(Place,verbose_name='地点',null=True,blank=True,default=None)
+    members=models.ManyToManyField(User,verbose_name='参与人',null=True,blank=True,default=None)
     cost=models.DecimalField('消费金额',max_digits=5, decimal_places=2,null=True,blank=True,default=None)
 
+    def __str__(self):
+        return '%s %s %s %s' % (self.time,self.place,self.members,self.cost)
+
+    class Meta:
+        verbose_name = '活动'
+        verbose_name_plural = verbose_name
+
+class Recharge_and_cost(models.Model):
+    event=models.ForeignKey(Event,verbose_name='事件',null=True,blank=True,default=None)
+    member=models.ForeignKey(User,verbose_name='参与人',null=True,blank=True,default=None)
+    cost=models.DecimalField(max_digits=5,decimal_places=2,null=True,blank=True,default=0.00)
+    recharge=models.ForeignKey(Recharge,verbose_name='参与人',null=True,blank=True,default=None)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.event,self.member,self.cost,self.recharge)
+
+    class Meta:
+        verbose_name = '充值及消费记录'
+        verbose_name_plural = verbose_name
+
+class Recharge(models.Model):
+    time = models.DateField('时间', null=True, blank=True, default=None)
+    member = models.ForeignKey(User, verbose_name='参与人', null=True, blank=True, default=None)
+    recharge = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, default=100.00)
+
+    def __str__(self):
+        return '%s %s %s' % (self.time,self.member,self.recharge)
+
+    class Meta:
+        verbose_name = '充值'
+        verbose_name_plural = verbose_name
