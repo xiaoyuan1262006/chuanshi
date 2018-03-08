76142891c8db9c98783c06dbc7d4851a6d4e1add
wblueboat <1351552638@qq.com>
2018-03-08 09:30:38 +0800

    初建表

--- badminton/cost/models.py
+++ badminton/cost/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    name=models.CharField('地点',max_length=50,null=True,blank=True,default=None)
+    
+class Event(models.Model):
+    time=models.DateField('时间',null=True,blank=True,default=None)
+    place=models.ForeignKey(verbose_name='地点',null=True,blank=True,default=None)
+    members=models.ManyToManyField(ManyToManyField,verbose_name='参与人',null=True,blank=True,default=None)
+    cost=models.DecimalField('消费金额',max_digits=5, decimal_places=2,null=True,blank=True,default=None)
+
