6fef44dd77c914d4cf1ec3c1d15d9a5ecb9a2ae9
CSkangzhiwei <1813863741@qq.com>
2018-03-09 14:38:55 +0800

    增加充值记录表头

--- badminton/cost/admin.py
+++ badminton/cost/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import Place,Event,Recharge
 
 class EventAdmin(admin.ModelAdmin):
+    # list_display = ['time', 'place', 'members', 'cost']
     #人员选择增加复选框
     filter_horizontal=('members',)
+class RechargeAdmin(admin.ModelAdmin):
+    list_display = ['time', 'member', 'recharge']
 
 admin.site.register(Place)
 admin.site.register(Event,EventAdmin)
-admin.site.register(Recharge)
+admin.site.register(Recharge,RechargeAdmin)
