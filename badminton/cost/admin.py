0c560486aae8321d3d341d8632a7f17c02e4fb63
CSkangzhiwei <1813863741@qq.com>
2018-03-09 13:53:48 +0800

    admin页面，活动人员选择增加复选框

--- badminton/cost/admin.py
+++ badminton/cost/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Place,Event,Recharge
 
+class EventAdmin(admin.ModelAdmin):
+    #人员选择增加复选框
+    filter_horizontal=('members',)
+
 admin.site.register(Place)
-admin.site.register(Event)
+admin.site.register(Event,EventAdmin)
 admin.site.register(Recharge)
