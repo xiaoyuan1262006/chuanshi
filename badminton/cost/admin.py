26ae20bb55285b9e8027777e80344f8368ee5d44
CSkangzhiwei <1813863741@qq.com>
2018-03-09 16:46:30 +0800

    1.增加充值及消费记录表 2.消费记录自动由活动登记时生成

--- badminton/cost/admin.py
+++ badminton/cost/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
-from .models import Place,Event,Recharge
+from .models import Place,Event,Recharge,Recharge_and_cost
 
 class EventAdmin(admin.ModelAdmin):
-    # list_display = ['time', 'place', 'members', 'cost']
-    #人员选择增加复选框
+    list_display = ['time', 'place', 'iteMembers', 'cost']
     filter_horizontal=('members',)
+    list_filter=('place', 'cost' )
+
+    def save_model(self, request, obj, form, change):
+        id=obj.id
+
+        if change:
+            for recharge_and_cost in obj.event_costs.all():
+                recharge_and_cost.delete()
+        else:
+            obj.save()
+        for member in form.cleaned_data['members']:
+            Recharge_and_cost.objects.create(
+                event=obj,
+                member=member,
+                cost=form.cleaned_data['cost']/form.cleaned_data['members'].count()
+            )
+        super().save_model(request, obj, form, change)
+
 class RechargeAdmin(admin.ModelAdmin):
     list_display = ['time', 'member', 'recharge']
+
+class Recharge_and_costAdmin(admin.ModelAdmin):
+    list_display = ['event', 'member', 'cost']
 
 admin.site.register(Place)
 admin.site.register(Event,EventAdmin)
 admin.site.register(Recharge,RechargeAdmin)
+admin.site.register(Recharge_and_cost,Recharge_and_costAdmin)
